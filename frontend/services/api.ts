08c6398daee0bca57a9de591dfd0eb51686fc649
yashwanth16-web <yashwanthkaramala8@gmail.com>
2026-07-15 13:52:28 +0530

    Configure API for production

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ??
+    "http://127.0.0.1:8000",
   headers: {
     "Content-Type": "application/json",
   },
